788f6f18be65c4aabc2631a673027ddbd591187c
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-06-08 21:53:34 +0100

    added github rest api package

--- src/Components/GitHubLang.js
+++ src/Components/GitHubLang.js
@@ -1,11 +1,28 @@
 import { useState, useEffect } from 'react'
+const { Octokit } = require('@octokit/rest')
 
 const GitHubLang = () => {
-  const [userId, setUserId] = useState({
-    userId: ''
+  const octokit = new Octokit({ auth: process.env.REACT_APP_GITHUB_API_TOKEN })
+  const [username, setUsername] = useState({
+    username: ''
   })
 
-  
+  const onChange = (element) => {
+    setUsername((prevState) => ({...prevState, [element.target.name]: element.target.value}))
+    console.log(username)
+  }
+
+  return(
+    <div>
+      <form id='githublang-form' className='lang-form'>
+        <label for='username'>Username</label><br></br>
+        <input type='text' 
+          name='username'
+          onChange={element => onChange(element)}  
+        />
+      </form>
+    </div>
+  )
 
 }
 
