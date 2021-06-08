203507882b7fa02641ab11e8be8b1554e5aab512
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-06-08 21:07:33 +0100

    added favourite language to nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,6 +6,7 @@ const NavBar = () => {
       <Link to='/' className='navlink'>Home</Link>
       <Link to='/character_sheet' className='navlink'>Character Sheet</Link>
       <Link to='/blog' className='navlink'>Blog</Link>
+      <Link to='/githublang' className='navlink'>Favourite Language</Link>
     </div>
   )
 }
